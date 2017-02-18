7f74cde00e12c73142bd29a0b545d0d1744ddbe8
Jason Lawless <jason.lawless@me.com>
2017-02-18 12:58:34 -0800

    1st JS/PHP lesson

---
+++ jason/programming/1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php
+		$age = 20;
+		$name = 'jason';
+		$lastname = 'lawless';
+		$amount = 20.50;
+		$br = '<br>'
+
+		echo $age.$br;
+		echo $name.$br;
+		echo $lastname.$br;
+		echo $amount.$br;
+		
+		echo $name . ' ' . $lastname;
+	?>
+</body>
+</html>
